3c3f615494bcca6a05e50694db1e13d3dc6d4bc9
parthkax70 <parthka.2005@gmail.com>
2022-12-30 20:26:27 +0530

    Projects

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/parthkax70/slect-back
+
+go 1.19
---
+++ pfourgo/parthka.go
@@ -0,0 +1,9 @@
+package pfourgo
+
+import "fmt"
+
+func Parthka()  {
+	fmt.Println("I Am Parthka!")
+	fmt.Println("I ❤️ Computers!")
+	fmt.Println("I Like Speed!")
+}
